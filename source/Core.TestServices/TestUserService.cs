bdb14025e81adb7e9595044d7dbf2c65eb625c26
Albert <albertserra@hotmail.co.uk>
2014-04-14 17:00:24 +0100

    Multitenancy support for TestUserService
    The password has to be the tenant + "_" + username.
    eg:
    tenant = myTenant
    user = myUser
    password = myTenant_myUser

--- source/Core.TestServices/TestUserService.cs
+++ source/Core.TestServices/TestUserService.cs
@@ -17,8 +17,37 @@ using Thinktecture.IdentityServer.Core.Services;
 
 namespace Thinktecture.IdentityServer.TestServices
 {
-    public class TestUserService : IUserService
+    public class TestUserService : IMultiTenantUserService, IUserService
     {
+        public Task<AuthenticateResult> AuthenticateLocalAsync(string tenant, string username, string password)
+        {
+            if (tenant + "_" + username != password) return Task.FromResult<AuthenticateResult>(null);
+
+            return Task.FromResult(new AuthenticateResult(username, username));
+        }
+
+        public Task<ExternalAuthenticateResult> AuthenticateExternalAsync(string tenant, string subject, ExternalIdentity user)
+        {
+            if (user == null)
+            {
+                return Task.FromResult<ExternalAuthenticateResult>(null);
+            }
+
+            var claims = user.Claims;
+            if (claims == null)
+            {
+                return Task.FromResult<ExternalAuthenticateResult>(null);
+            }
+
+            var name = claims.FirstOrDefault(x => x.Type == ClaimTypes.Name || x.Type == Constants.ClaimTypes.Name);
+            if (name == null)
+            {
+                return null;
+            }
+
+            return Task.FromResult(new ExternalAuthenticateResult(user.Provider.Name, Guid.NewGuid().ToString("D"), name.Value));
+        }
+
         public Task<IEnumerable<Claim>> GetProfileDataAsync(string sub, IEnumerable<string> requestedClaimTypes = null)
         {
             var claims = new List<Claim>();
